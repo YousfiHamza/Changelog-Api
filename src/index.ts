8f87a15a2ba1957bf8d5089e4b33831592bee25f
YousfiHamza <yousfihamza22@gmail.com>
2023-10-03 10:24:07 +0200

    Routes & Middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-import express, { Application, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Application } from 'express';
+import dotenv from 'dotenv'; // For env File
+import morgan from 'morgan'; // For logging
+import cors from 'cors'; // For Cross Origin Resource Sharing
+
+import router from './router';
 
 //For env File 
 dotenv.config();
 
+// PORT
 const PORT = process.env.PORT || 1234;
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200)
-    res.json({message: 'Hello World from express'});
-});
+app.use(cors()); // For Cross Origin Resource Sharing
+
+app.use(express.json()); // For parsing application/json
+
+app.use(morgan('dev')); // For logging
+
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+app.use('/api', router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} ... 🚀`);
